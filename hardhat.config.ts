5b624b8085ab03b8b847abb02fd5c1fcdc8c4c88
parshviSri <parshvisri@gmail.com>
2023-04-02 22:51:00 +0530

    Add ing mumbai testnet
    Signed-off-by: parshviSri <parshvisri@gmail.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
+let account:string = `0x${process.env.NEXT_PUBLIC_PRIVATE_KEY}`||''
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
   networks: {
     hardhat: {
       chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`,
+      accounts: [account],
     },
   },
 };
